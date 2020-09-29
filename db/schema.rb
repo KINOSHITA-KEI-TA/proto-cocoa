9dbb18cc1aaa06f1b49ac871651ae7429c49d61b
KINOSHITA-KEI-TA <kei.quality3@gmail.com>
2020-09-30 00:04:26 +0900

    体温イベント（日記表示）

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2020_09_24_121525) do
     t.integer "user_id"
     t.integer "event_id"
     t.text "name", null: false
-    t.integer "temperature_id", null: false
+    t.integer "temperature_id"
     t.text "text", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
